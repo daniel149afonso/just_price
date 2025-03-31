a4aa9085d3c7eff3b64085a05ea0e7d94460002a
Daniel <daniel-congo2@hotmail.com>
2025-04-01 00:18:55 +0200

    add project juste price

---
+++ index.js
@@ -0,0 +1,69 @@
+let input = document.querySelector("input");
+let error = document.querySelector("small");
+let formulaire = document.querySelector("#formulaire");
+let button = document.querySelector("button");
+
+let rdn = Math.floor(Math.random()*1001);
+let count = 0;
+let chosenNumber;
+
+//affiche le juste prix dans la console
+console.log("The price: "+rdn);
+
+//#1 Check if we enter number côté client
+input.addEventListener('keyup', ()=>{
+    //check if is a number
+    if(isNaN(input.value)){
+        //Display the error
+        error.style.display = "inline";
+    }
+    else{
+        //Display nothing
+        error.style.display = "none";
+    }
+})
+//#2 Check if we enter number côté serveur
+formulaire.addEventListener('submit', (e) =>{
+    //empeche d'envoyer la valeur du formulaire côté serveur ce qu'on ne veut pas
+    e.preventDefault();
+    //si est pas un nombre
+    if(isNaN(input.value)|| input.value == ""){
+        input.style.borderColor = "red";
+    }
+    //si est un nombre
+    else{
+        count++;
+        input.style.borderColor = "green";
+        chosenNumber = input.value;
+        input.value= '';
+        //
+        displayInstruction(chosenNumber);
+        
+    }
+
+})
+
+function displayInstruction(chosenNumber){
+    //Créer la div instruction
+    let instruction = document.createElement("div");
+    chosenNumber = parseInt(chosenNumber);//convert to int
+        if(chosenNumber>rdn){
+            //ajoute la class moins
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) C'est moins !";
+            instruction.className = "instruction moins";
+        }
+        else if(chosenNumber<rdn){
+            //ajoute la class plus
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) C'est plus !";
+            instruction.className = "instruction plus";
+        }
+        else{
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) Félicitations vous avez trouvé le juste prix !";
+            instruction.className = "instruction fini";
+            //desactive l'input quand on trouve le prix
+            input.disabled = true;
+        }
+    //Ajoute l'instruction dans la page en fonction de la class
+    //prepend ajoute la div avant l'element (défilement vers le haut) différent de append
+    document.querySelector("#instructions").prepend(instruction);
+}
