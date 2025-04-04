87a1ed35c78314c6bb062cced07cee3cd911bba7
daniel <daniel-congo2@hotmail.com>
2025-04-04 02:54:26 +0200

    correction white space are refused

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ formulaire.addEventListener('submit', (e) =>{
     //empeche d'envoyer la valeur du formulaire côté serveur ce qu'on ne veut pas
     e.preventDefault();
     //si est pas un nombre
-    if(isNaN(input.value)|| input.value == ""){
+    if(isNaN(input.value) || input.value == "" || input.value.indexOf(' ') >= 0){
         input.style.borderColor = "red";
     }
     //si est un nombre
@@ -49,16 +49,16 @@ function displayInstruction(chosenNumber){
     chosenNumber = parseInt(chosenNumber);//convert to int
         if(chosenNumber>rdn){
             //ajoute la class moins
-            instruction.textContent = "#" + count + "( " + chosenNumber + " ) C'est moins !";
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) It's smaller !";
             instruction.className = "instruction moins";
         }
         else if(chosenNumber<rdn){
             //ajoute la class plus
-            instruction.textContent = "#" + count + "( " + chosenNumber + " ) C'est plus !";
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) It's greather !";
             instruction.className = "instruction plus";
         }
         else{
-            instruction.textContent = "#" + count + "( " + chosenNumber + " ) Félicitations vous avez trouvé le juste prix !";
+            instruction.textContent = "#" + count + "( " + chosenNumber + " ) Congratulations you have found the price !";
             instruction.className = "instruction fini";
             //desactive l'input quand on trouve le prix
             input.disabled = true;
